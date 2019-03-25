ff7795f858da283e267d3621548a7d5b03b3b116
idalgo99produo <zashihin_ilya@mail.ru>
2019-03-25 22:59:41 +1000

    Testing random "int" code

--- testrand.go
+++ testrand.go
@@ -22,14 +22,28 @@ func GenerateRandomString(s int) (string, error) {
 }
 
 func main() {
-	//	token, err := GenerateRandomString(32)
+	//	token, err := GenerateRandomString(8)
+	//	if err != nil {
+	//		return
+	//	}
+	//	fmt.Println(token)
 	//	var t int
 	//	fmt.Println("Enter number: ")
 	//	fmt.Scan(&t)
 	//	token, err := GenerateRandomBytes(t)
 	token, err := GenerateRandomBytes(8)
 	if err != nil {
+		return
 	}
+
+	y := int(token[1])
+
+	fmt.Println(y)
+
+	//	x, er := strconv.Atoi(string(token[1]))
+	//	if er != nil {
+	//	}
+	//	fmt.Println(x)
 	//	i, err := strconv.ParseInt(token, 10, 64)
 	//	if err != nil {
 	//		return
@@ -37,5 +51,5 @@ func main() {
 	//	fmt.Println(i)
 	//	data := binary.BigEndian.Uint64(token)
 	//	fmt.Println(data)
-	fmt.Println(token[1])
+	//	fmt.Println(token[1])
 }
